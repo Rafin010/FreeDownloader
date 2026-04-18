f45e545cc8bc031761dd95192c804ae1c3c3aa72
Rafin <iarafin010@gmail.com>
2026-04-18 22:38:32 +0600

    Update frontend and deploy latest code (secrets removed)

--- fb_downloader/static/js/sw.js
+++ fb_downloader/static/js/sw.js
@@ -1,7 +1,16 @@
-// AD SCRIPT DISABLED self.options = {
-// AD SCRIPT DISABLED     "domain": "5gvci.com",
-// AD SCRIPT DISABLED     "zoneId": 10863620
-// AD SCRIPT DISABLED }
-// AD SCRIPT DISABLED self.lary = ""
-// AD SCRIPT DISABLED importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
-// AD SCRIPT DISABLED 
+// AD SCRIPT DISABLED - Service Worker replaced with self-unregister script
+// This will unregister the old Monetag ad service worker from all visitor browsers
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url));
+    });
+});
