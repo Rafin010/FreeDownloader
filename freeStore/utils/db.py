46adb90ee5902118f764ea071cfc9db585af652d
Ifat Ahmed Rafin <111105988+Rafin010@users.noreply.github.com>
2026-08-14 03:01:29 +0600

    Support DB_PORT for cloud DBs like Aiven

--- freeStore/utils/db.py
+++ freeStore/utils/db.py
@@ -9,6 +9,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME", "downloader_analytics")
+DB_PORT = os.getenv("DB_PORT", "3306")
 
 def get_connection():
     """Get a direct database connection."""
@@ -17,7 +18,8 @@ def get_connection():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME
+            database=DB_NAME,
+            port=int(DB_PORT)
         )
         return connection
     except Error as e:
